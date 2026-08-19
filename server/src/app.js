fc40801686003e335b16c695214aab23c73793d5
ysuovnii <ysuovnii@gmail.com>
2026-08-19 18:50:53 +0530

    remove client/server

--- server/src/app.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import accessRouter from './access/access.route.js';
-const app = express();
-
-app.use(express.json());
-app.use('/api/access', accessRouter);
-
-export default app;
